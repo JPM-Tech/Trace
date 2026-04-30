908dd1680b44238334060a87b5d114450033ff98
JPM-Tech <chase@jpmtech.io>
2026-04-30 15:05:01 -0600

    disable auto-off screen timer

--- Trace/TraceApp.swift
+++ Trace/TraceApp.swift
@@ -2,9 +2,14 @@ import SwiftUI
 
 @main
 struct TraceApp: App {
+    @Environment(\.scenePhase) private var scenePhase
+
     var body: some Scene {
         WindowGroup {
             ContentView()
+        }
+        .onChange(of: scenePhase) { phase in
+            UIApplication.shared.isIdleTimerDisabled = (phase == .active)
         }
     }
 }
